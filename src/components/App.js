ccf501580a5d62842bad7f966485832b1ad7be21
Aniket240700 <104688183+Aniket240700@users.noreply.github.com>
2022-09-05 21:02:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 const App = () => {
   
+  const [button,setButton] = useState('polo');
+  const [heading,setHeading] = useState('marco');
+  
+  function toggle(){
+  if(button == 'polo'){
+  setButton('marco');
+  }
+    else{
+    setButton('polo');
+    }
+    if(heading == 'polo'){
+    setHeading('marco');
+    }
+    else{
+    setHeading('polo');
+    }
+  }
+  
   return (
     <div id="main">
-
+     <h1>{heading}</h1>
+     <button onClick={toggle}>{button}</button>
     </div>
   )
 }
